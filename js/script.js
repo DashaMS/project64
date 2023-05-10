2886b4cd536c7b83e23e2bd1d8e58411dc5f7b55
dasha <shyshman.gp@gmail.com>
2023-05-10 12:21:42 +0300

    firtst commit

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', function () {
     tabsContent[i].classList.add('show', 'fade');
     tabsContent[i].classList.remove('hide');
     tabs[i].classList.add('tabheader__item_active');
-  }
+  } 
 
   hideTabContent();
   showTabContent();
